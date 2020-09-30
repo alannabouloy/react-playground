daf0ee1b82afeca2a9538ebd8823f89d6ef86129
Alanna Bouloy <lanihope@gmail.com>
2020-09-30 12:24:57 -0400

    hello world drill completed

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default class HelloWorld extends React.Component {
+    state = {
+        who: 'world'
+    }
+    handleWorldClick = () => {
+        this.setState({
+            who: 'world'
+        });
+    }
+
+    handleFriendClick = () => {
+        this.setState({
+            who: 'friend'
+        });
+    }
+
+    handleReactClick = () => {
+        this.setState({
+            who: 'react'
+        });
+    }
+    render(){
+        return (
+            <div>
+                <p>Hello {this.state.who}!</p>
+
+                <button onClick={this.handleWorldClick}>World</button>
+                <button onClick={this.handleFriendClick}>Friend</button>
+                <button onClick={this.handleReactClick}>React</button>
+            </div>
+        )
+    }
+}
